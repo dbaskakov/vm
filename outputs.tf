8b99afe9a8253ffd013ab7b84838989cf317c4dc
Dmitry Baskakov <dm.baskakov@yandex.ru>
2020-12-03 11:01:03 +0300

    fix output module

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 output "node_internal_ip" {
-  value = google_compute_instance.node.*.network_interface.0.network_ip
+  value = module.google_compute_instance.node.*.network_interface.0.network_ip
 }
 
 output "node_external_ip" {
-  value = google_compute_instance.node.*.network_interface.0.access_config.0.nat_ip
+  value = module.google_compute_instance.node.*.network_interface.0.access_config.0.nat_ip
 }
 
